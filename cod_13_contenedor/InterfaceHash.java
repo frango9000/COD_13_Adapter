216a8b898f1c65aac4fc7dc58e4fc916cb5fea36
fsancheztemprano <fsancheztemprano@danielcastelao.org>
2019-02-26 10:47:07 +0100

    listas y hashmap

--- cod_13_contenedor/InterfaceHash.java
+++
@@ -1,8 +0,0 @@
-package cod_13_contenedor;
-//@author fsancheztemprano
-public interface InterfaceHash {
-    public void putValue(String elemento);
-    
-    public String pushValue(int key);
-
-}
